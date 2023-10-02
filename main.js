7d1171ccd37a3f9094be1e45e71bd4c70f152166
DilawarBaig <dilawaralibaig@gmail.com>
2023-10-02 20:58:44 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var magicians = ["Chriss Angle", "Dynamo", "David Blaine"];
+function show_magicians(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        console.log(magicians[i]);
+    }
+}
+show_magicians(magicians);
---
+++ main.ts
@@ -0,0 +1,8 @@
+let magicians:string[] = ["Chriss Angle","Dynamo","David Blaine"]
+
+function show_magicians (magicians:string[]):void {
+  for (let i= 0; i<magicians.length;i++) {
+    console.log(magicians[i])
+  }
+}
+show_magicians(magicians)
